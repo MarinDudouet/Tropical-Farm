781d97d04e19f748d62c21f16309b6f7ed78f85c
Héloïse <118915812+HeloiseMCH@users.noreply.github.com>
2023-07-11 12:59:58 +0100

    commit

--- java.js
+++ java.js
@@ -1,2 +1,26 @@
+//Caroussel
 
+var carousel = document.querySelector('.carousel-inner');
+    var currentImageIndex = 0;
+    var imageCount = carousel.childElementCount;
+    var imageWidth = carousel.firstElementChild.clientWidth;
+    var slideInterval = setInterval(nextImage, 5000); // Défilement automatique toutes les 1,5 seconde
+    
+    function previousImage() {
+      clearInterval(slideInterval); // Arrêter le défilement automatique lors de la navigation manuelle
+      if (currentImageIndex > 0) {
+        currentImageIndex--;
+        carousel.style.left = -currentImageIndex * imageWidth + 'px';
+      }
+    }
+    
+    function nextImage() {
+      if (currentImageIndex < imageCount - 1) {
+        currentImageIndex++;
+        carousel.style.left = -currentImageIndex * imageWidth + 'px';
+      } else {
+        currentImageIndex = 0;
+        carousel.style.left = 0;
+      }
+    }
   
