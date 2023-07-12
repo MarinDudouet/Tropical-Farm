95a766eacba2cd8bb360e933defd99479f7cde66
Héloïse <118915812+HeloiseMCH@users.noreply.github.com>
2023-07-12 13:06:16 +0100

    je commit

--- login.php
+++ login.php
@@ -1,4 +1,7 @@
 <?php
+
+session_start();
+
     $serveur = "localhost";
     $dbname = "tropicalfarm";
     $user = "root";
@@ -14,6 +17,8 @@
         $dbco = new PDO("mysql:host=$serveur;dbname=$dbname",$user,$pass);
         $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+        //Seller
+
         $query = "SELECT * FROM seller WHERE username = :username AND password = :password";
         $stmt = $dbco->prepare($query);
         $stmt->bindParam(':username', $username);
@@ -22,11 +27,86 @@
         
         if ($stmt->rowCount() > 0) {
         // Connexion réussie
-        echo 'success';
-    } else {
-        // Identifiants invalides
-        echo "error";
-}
+        header('Location: index.html');
+        $_SESSION["role"]="seller";
+        $_SESSION["name"]=$row['name'];
+        $_SESSION["street"]=$row['street'];
+        $_SESSION["flat"]=$row['flat'];
+        $_SESSION["city"]=$row['city'];
+        $_SESSION["state"]=$row['state'];
+        $_SESSION["postcode"]=$row['postcode'];
+        $_SESSION["phone"]=$row['phone'];
+        $_SESSION["typecard"]=$row['card'];
+        $_SESSION["cardnumber"]=$row['cardnumber'];
+        $_SESSION["expirationmonth"]=$row['monthexpiration'];
+        $_SESSION["expirationyear"]=$row['yearexpiration'];
+        $_SESSION["cvc"]=$row['cvc'];
+
+        exit();
+        } 
+
+        //Buyer
+
+        $query = "SELECT * FROM seller WHERE username = :username AND password = :password";
+        $stmt = $dbco->prepare($query);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':password', $password);
+        $stmt->execute();
+        
+        if ($stmt->rowCount() > 0) {
+        // Connexion réussie
+        header('Location: index.html');
+        $_SESSION["role"]="buyer";
+        $_SESSION["name"]=$row['name'];
+        $_SESSION["street"]=$row['street'];
+        $_SESSION["flat"]=$row['flat'];
+        $_SESSION["city"]=$row['city'];
+        $_SESSION["state"]=$row['state'];
+        $_SESSION["postcode"]=$row['postcode'];
+        $_SESSION["phone"]=$row['phone'];
+        $_SESSION["typecard"]=$row['card'];
+        $_SESSION["cardnumber"]=$row['cardnumber'];
+        $_SESSION["expirationmonth"]=$row['monthexpiration'];
+        $_SESSION["expirationyear"]=$row['yearexpiration'];
+        $_SESSION["cvc"]=$row['cvc'];
+
+        exit();
+        } 
+
+        //Admin
+
+        $query = "SELECT * FROM seller WHERE username = :username AND password = :password";
+        $stmt = $dbco->prepare($query);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':password', $password);
+        $stmt->execute();
+        
+        if ($stmt->rowCount() > 0) {
+        // Connexion réussie
+        header('Location: index.html');
+        $_SESSION["role"]="admin";
+        $_SESSION["name"]=$row['name'];
+        $_SESSION["street"]=$row['street'];
+        $_SESSION["flat"]=$row['flat'];
+        $_SESSION["city"]=$row['city'];
+        $_SESSION["state"]=$row['state'];
+        $_SESSION["postcode"]=$row['postcode'];
+        $_SESSION["phone"]=$row['phone'];
+        $_SESSION["typecard"]=$row['card'];
+        $_SESSION["cardnumber"]=$row['cardnumber'];
+        $_SESSION["expirationmonth"]=$row['monthexpiration'];
+        $_SESSION["expirationyear"]=$row['yearexpiration'];
+        $_SESSION["cvc"]=$row['cvc'];
+
+        exit();
+        } 
+
+        else {
+            // Identifiants invalides
+            header('Location: loginerror.html');
+            exit();
+            }
+
 }
     catch(PDOException $e){
         echo 'Impossible de traiter les données. Erreur : '.$e->getMessage();
